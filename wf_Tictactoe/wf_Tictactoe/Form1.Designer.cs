582e33a1f382afa4be9d8913c5e1a50468471d16
gavrielRa <gabira2903@gmail.com>
2016-08-28 17:21:47 +0300

    new options to the interface

--- wf_Tictactoe/wf_Tictactoe/Form1.Designer.cs
+++ wf_Tictactoe/wf_Tictactoe/Form1.Designer.cs
@@ -43,6 +43,13 @@
             this.C1 = new System.Windows.Forms.Button();
             this.C2 = new System.Windows.Forms.Button();
             this.C3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.x_count = new System.Windows.Forms.Label();
+            this.draw_count = new System.Windows.Forms.Label();
+            this.o_count = new System.Windows.Forms.Label();
+            this.resetAllFieldsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,6 +69,7 @@
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.newGameToolStripMenuItem,
+            this.resetAllFieldsToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
@@ -106,6 +114,8 @@
             this.A1.TabIndex = 1;
             this.A1.UseVisualStyleBackColor = true;
             this.A1.Click += new System.EventHandler(this.button_click);
+            this.A1.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.A1.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // A2
             // 
@@ -117,6 +127,8 @@
             this.A2.TabIndex = 2;
             this.A2.UseVisualStyleBackColor = true;
             this.A2.Click += new System.EventHandler(this.button_click);
+            this.A2.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.A2.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // A3
             // 
@@ -128,6 +140,8 @@
             this.A3.TabIndex = 3;
             this.A3.UseVisualStyleBackColor = true;
             this.A3.Click += new System.EventHandler(this.button_click);
+            this.A3.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.A3.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // B1
             // 
@@ -139,6 +153,8 @@
             this.B1.TabIndex = 4;
             this.B1.UseVisualStyleBackColor = true;
             this.B1.Click += new System.EventHandler(this.button_click);
+            this.B1.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.B1.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // B2
             // 
@@ -150,6 +166,8 @@
             this.B2.TabIndex = 5;
             this.B2.UseVisualStyleBackColor = true;
             this.B2.Click += new System.EventHandler(this.button_click);
+            this.B2.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.B2.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // B3
             // 
@@ -161,6 +179,8 @@
             this.B3.TabIndex = 6;
             this.B3.UseVisualStyleBackColor = true;
             this.B3.Click += new System.EventHandler(this.button_click);
+            this.B3.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.B3.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // C1
             // 
@@ -172,6 +192,8 @@
             this.C1.TabIndex = 7;
             this.C1.UseVisualStyleBackColor = true;
             this.C1.Click += new System.EventHandler(this.button_click);
+            this.C1.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.C1.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // C2
             // 
@@ -183,6 +205,8 @@
             this.C2.TabIndex = 8;
             this.C2.UseVisualStyleBackColor = true;
             this.C2.Click += new System.EventHandler(this.button_click);
+            this.C2.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.C2.MouseLeave += new System.EventHandler(this.mouse_leave);
             // 
             // C3
             // 
@@ -194,12 +218,84 @@
             this.C3.TabIndex = 9;
             this.C3.UseVisualStyleBackColor = true;
             this.C3.Click += new System.EventHandler(this.button_click);
+            this.C3.MouseEnter += new System.EventHandler(this.mouse_enter);
+            this.C3.MouseLeave += new System.EventHandler(this.mouse_leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(27, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X Win";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(188, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "O Win ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(113, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Draw";
+            // 
+            // x_count
+            // 
+            this.x_count.AutoSize = true;
+            this.x_count.Location = new System.Drawing.Point(44, 316);
+            this.x_count.Name = "x_count";
+            this.x_count.Size = new System.Drawing.Size(16, 17);
+            this.x_count.TabIndex = 13;
+            this.x_count.Text = "0";
+            // 
+            // draw_count
+            // 
+            this.draw_count.AutoSize = true;
+            this.draw_count.Location = new System.Drawing.Point(126, 316);
+            this.draw_count.Name = "draw_count";
+            this.draw_count.Size = new System.Drawing.Size(16, 17);
+            this.draw_count.TabIndex = 14;
+            this.draw_count.Text = "0";
+            // 
+            // o_count
+            // 
+            this.o_count.AutoSize = true;
+            this.o_count.Location = new System.Drawing.Point(204, 316);
+            this.o_count.Name = "o_count";
+            this.o_count.Size = new System.Drawing.Size(16, 17);
+            this.o_count.TabIndex = 15;
+            this.o_count.Text = "0";
+            // 
+            // resetAllFieldsToolStripMenuItem
+            // 
+            this.resetAllFieldsToolStripMenuItem.Name = "resetAllFieldsToolStripMenuItem";
+            this.resetAllFieldsToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.resetAllFieldsToolStripMenuItem.Text = "Reset All Fields";
+            this.resetAllFieldsToolStripMenuItem.Click += new System.EventHandler(this.resetAllFieldsToolStripMenuItem_Click);
             // 
             // TixTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(267, 291);
+            this.ClientSize = new System.Drawing.Size(267, 355);
+            this.Controls.Add(this.o_count);
+            this.Controls.Add(this.draw_count);
+            this.Controls.Add(this.x_count);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.C3);
             this.Controls.Add(this.C2);
             this.Controls.Add(this.C1);
@@ -240,6 +336,13 @@
         private System.Windows.Forms.Button C1;
         private System.Windows.Forms.Button C2;
         private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label x_count;
+        private System.Windows.Forms.Label draw_count;
+        private System.Windows.Forms.Label o_count;
+        private System.Windows.Forms.ToolStripMenuItem resetAllFieldsToolStripMenuItem;
     }
 }
 
--- wf_Tictactoe/wf_Tictactoe/Form1.cs
+++ wf_Tictactoe/wf_Tictactoe/Form1.cs
@@ -13,7 +13,7 @@ namespace wf_Tictactoe
     public partial class TixTacToe : Form
     {
 
-        bool turn = true;//true = x, false = y;
+        bool turn = true;//true = X, false = O;
         int turn_count = 0;
 
 
@@ -38,16 +38,18 @@ namespace wf_Tictactoe
             turn = true;
             turn_count = 0;
 
-            try
+
+            foreach (Control c in Controls)
             {
-                foreach (Control c in Controls)
+                try
                 {
                     Button b = (Button)c;
                     b.Enabled = true;
                     b.Text = "";
                 }
-            }//end try
-            catch { };
+                catch { };
+
+            }
         }
 
         //button click actions
@@ -55,9 +57,9 @@ namespace wf_Tictactoe
         {
             Button b = (Button)sender;
             if (turn)
-                b.Text = "O";
-            else
                 b.Text = "X";
+            else
+                b.Text = "O";
             turn = !turn;
             b.Enabled = false;
             turn_count++;
@@ -78,15 +80,15 @@ namespace wf_Tictactoe
                 ther_is_a_winner = true;
 
             //vertical check
-            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
                 ther_is_a_winner = true;
-            else if((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
                 ther_is_a_winner = true;
             else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
                 ther_is_a_winner = true;
 
             //diagonal check
-            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
                 ther_is_a_winner = true;
             else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
                 ther_is_a_winner = true;
@@ -96,16 +98,23 @@ namespace wf_Tictactoe
             {
                 String winner = "";
                 if (turn)
-                    winner = "X";
-                else
+                {
                     winner = "O";
+                    o_count.Text =(Int32.Parse(o_count.Text) + 1).ToString();
+                }
+                else
+                {
+                    winner = "X";
+                    x_count.Text = (Int32.Parse(x_count.Text) + 1).ToString();
 
+                }
                 MessageBox.Show("The Winner is: "+winner, "Winner");
                 disableButtons();
             }//end if
             else if(turn_count == 9)
             {
                 MessageBox.Show("The is a Draw!!!", "Winner");
+                draw_count.Text = (Int32.Parse(draw_count.Text) + 1).ToString();
             }
 
 
@@ -113,17 +122,47 @@ namespace wf_Tictactoe
         //disable all buttons 
         private void disableButtons()
         {
-            try
+
+            foreach (Control c in Controls)
             {
-                foreach (Control c in Controls)
+                try
                 {
                     Button b = (Button)c;
                     b.Enabled = false;
                 }
-            }//end try
-            catch { };
+                catch { };
+
+            }
         }
 
- 
+
+
+
+        private void mouse_enter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                if (turn)
+                    b.Text = "X";
+                else
+                    b.Text = "O";
+            }
+        }
+
+        private void mouse_leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+                b.Text = "";
+        }
+
+        private void resetAllFieldsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newGameToolStripMenuItem_Click(sender,e);
+            x_count.Text = "0";
+            o_count.Text = "0";
+            draw_count.Text = "0";
+        }
     }
 }
